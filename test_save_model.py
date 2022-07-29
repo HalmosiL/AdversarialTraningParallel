e85387d6503c142127f294e1cb4f044dcaf3deca
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-07-29 13:19:26 +0200

    Delete test_save_model.py

--- test_save_model.py
+++
@@ -1,5 +0,0 @@
-from ModelLodaer import resnet_slice_model, get_resnet18_hourglass
-import torch
-
-model = get_resnet18_hourglass("cpu", encoder_weights=None)
-torch.save(model.state_dict(), "./model_cache/model_1.pt")
